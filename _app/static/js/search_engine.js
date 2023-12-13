b0e89990ba5538dcdad56b22cdd741267e89a6f7
Henrique <34416111+henriquecgarcia@users.noreply.github.com>
2023-12-12 22:23:48 -0300

    Refactor search functionality and improve code structure

--- _app/static/js/search_engine.js
+++ _app/static/js/search_engine.js
@@ -1,19 +1,18 @@
-const display = $('#display-chars');
-const all_chars = display.children();
-
-$().ready(function() {
-	let childs = display.children();
-	$('#char-search').keyup(function() {
-		var query;
-		query = $(this).val();
-		childs.each(function() {
-			var child;
-			child = $(this);
-			if (child.text().toLowerCase().indexOf(query.toLowerCase()) === -1) {
-				child.hide();
-			} else {
-				child.show();
+$(document).ready(function(){
+	$('#char-search').keyup(function(){
+		var search = $(this).val();
+		$('.char').each(function(){
+			var text = $(this).find('h1').find("a").text();
+			if(search == '' || text.toLowerCase().indexOf(search) != -1 || text.toLowerCase() == search){
+				$(this).show();
+			}else{
+				$(this).hide();
 			}
 		});
+	});
+
+	$(".char").click(function(){
+		var id = $(this).attr('id');
+		window.location.href = "/func/" + id;
 	});
 });
